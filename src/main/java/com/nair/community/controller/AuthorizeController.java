429ce8fcc8dc50dbfb3670479f3343222400e67f
Nairorax <Nairorax@gmail.com>
2020-08-20 22:15:45 +0800

    delete MySql

--- src/main/java/com/nair/community/controller/AuthorizeController.java
+++ src/main/java/com/nair/community/controller/AuthorizeController.java
@@ -5,6 +5,7 @@ import com.nair.community.dto.GithubUser;
 import com.nair.community.mapper.UserMapper;
 import com.nair.community.model.User;
 import com.nair.community.provider.GithubProvider;
+import com.nair.community.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
@@ -33,6 +35,9 @@ public class AuthorizeController {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name= "code" ) String code,
                            @RequestParam(name = "state") String state,
@@ -51,15 +56,24 @@ public class AuthorizeController {
             user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
-            user.setGmtCreate(System.currentTimeMillis());
-            user.setGmtModified(user.getGmtCreate());
             user.setAvatarUrl(githubUser.getAvatar_url());
-            userMapper.insert(user);
+            userService.createOrUpdate(user);
             response.addCookie(new Cookie("token",token));
             return "redirect:/";
         }else {
             // 登录失败，重新登陆
             return "redirect:/";
         }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request,
+                         HttpServletResponse response){
+        request.getSession().removeAttribute("user");
+        // 用空白cookie 覆盖原本cookie 达到删除目的
+        Cookie cookie = new Cookie("token",null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return "redirect:/";
     }
 }
